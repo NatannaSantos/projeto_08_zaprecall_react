79a0f9c3b778e99f8cad4c6de8670d532bc2d359
thiagoribb <thiribeiro142@gmail.com>
2021-12-16 11:35:27 -0300

    Preparando revisão de código

--- src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import logoMini from "../../img/logoMini.png";
-
-export default function Header() {
-    return (
-        <header>
-            <img src={logoMini} />
-        </header>
-    );
-}
